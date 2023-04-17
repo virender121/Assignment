a03d99efb20be4e4bbff0ad2e2bfbccbcd763191
virender121 <119596715+virender121@users.noreply.github.com>
2023-04-18 04:24:38 +0530

    changes

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,32 +1,32 @@
 
 
-import React from 'react'
+import React,{useState} from 'react'
 import "./Dashboard.css"
 import LeftSection from '../../Section/LeftSection/LeftSection'              
 import RightSection from '../../Section/RightSection/RightSection'
 import Header from '../../Section/Header/Header'
 import { Navigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import Footer from '../../Section/Footer/Footer'
+// import Footer from '../../Section/Footer/Footer'
 
 const Dashboard = () => {
+
   console.log("world")
   const user = useSelector(state => state)
 console.log(user,"====")
   if (!user) {
     return <Navigate to="/" replace />;
   }
+
+
   
   return (
     <div className="main">
-      <input type="checkbox" id="toggleBtn" />
-      <label htmlFor="toggleBtn" className="menu-icon">
-        <Header />
-      </label>
+     
       
       <div className='leftsection'><LeftSection/></div>
 
-      <div className='rightsection'>
+      <div className='rightsection'> <Header />
         <RightSection/>
       </div>
     </div>
--- src/Section/Header/Header.js
+++ src/Section/Header/Header.js
@@ -1,18 +1,54 @@
-import React from 'react'
-import "./Header.css"
-import {ImSearch} from 'react-icons/im';
+import React, { useState, useEffect } from 'react';
+import './Header.css';
+import { ImSearch } from 'react-icons/im';
 import Avatar from '@mui/material/Avatar';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
 const Header = () => {
+  const [click, setClick] = useState(false);
+  const [color, setColor] = useState(false);
+
+  const handleClick = () => {
+    setClick(!click);
+  };
+
+  useEffect(() => {
+    const changeColor = () => {
+      if (window.scrollY >= 100) {
+        setColor(true);
+      } else {
+        setColor(false);
+      }
+    };
+    window.addEventListener('scroll', changeColor);
+    return () => {
+      window.removeEventListener('scroll', changeColor);
+    };
+  }, []);
+
   return (
-    <div className= 'header'>
-        <div><ImSearch/></div> 
-      <div>
-      <Avatar src="https://cdn.batesvilletechnology.com/fh_live/13000/13002/images/obituaries/6827690.jpg" 
-      alt="Cindy Baker"/>
-        {/* <button id="toggleBtn">&#9776;</button> */}
+    <div className={`header ${color ? 'header--colored' : ''}`}>
+      <ImSearch className="header__search-icon" />
+      <Avatar
+        src="https://cdn.batesvilletechnology.com/fh_live/13000/13002/images/obituaries/6827690.jpg"
+        alt="Cindy Baker"
+      />
+      <div className="header__hamburger" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: '#fff' }} />
+        ) : (
+          <FaBars size={20} style={{ color: '#fff' }} />
+        )}
+      </div>
+      <div className={`header__left-section ${click ? 'header__left-section--open' : ''}`}>
+        <ul className="header__menu">
+          <li className="header__menu-item">Menu Item 1</li>
+          <li className="header__menu-item">Menu Item 2</li>
+          <li className="header__menu-item">Menu Item 3</li>
+        </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
